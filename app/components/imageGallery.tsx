e7b44ceaee0b81b219d2b69e364e07a5b927c2b6
Eirik Kirkeli <knirkeli88@msn.com>
2024-02-19 13:59:03 +0100

    Refactor imageGallery component to handle big image display

--- app/components/imageGallery.tsx
+++ app/components/imageGallery.tsx
@@ -1,13 +1,19 @@
+"use client";
 import Image from "next/image"
 import { urlFor } from "../lib/sanity"
+import { useState } from "react";
 
 interface iAppProps {
     images: any;
 }
 
-export default function imageGallery({ images }: iAppProps) {
+export default function ImageGallery({ images }: iAppProps) {
+    const [bigImage, setBigImage] = useState(images[0]);
+    const handleSmallImageClick = (image: any) => {
+        setBigImage(image);
+    }
 
-    return ( <div className="grid-gap-4 lg:grid-cols-5">
+    return ( <div className="">
         <div className="order-last flex gap-4 lg:order-none lg:flex-col">
             {images.map((image: any, idx: any) => (                
                 <div key={idx} className="overflow-hidden rounded-lg bg-gray-100">
@@ -15,10 +21,18 @@ export default function imageGallery({ images }: iAppProps) {
                     width={200} 
                     height={200} 
                     alt="photo" 
-                    className="h-full w-full object-cover object-center cursor-pointer priority" 
+                    className="h-full w-full object-cover object-center cursor-pointer"
+                    onClick={() => handleSmallImageClick(image)}
                     />
                 </div>
             ))}
+        </div>
+        <div className="relative overflow-hidden rounded-lg bg-gray-100 lg:col-span-4 gap-4">
+            <Image src={urlFor(bigImage).url()} 
+            alt="People skiing" 
+            width={500} 
+            height={500} 
+            className="h-full w-full object-cover object-center" />
         </div>
     </div>
     );
